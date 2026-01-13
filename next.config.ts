56aa7a680cecf3d5acce9693c02ea3f5247d199b
Sam <veesong.szu@gmail.com>
2026-01-13 10:52:46 +0800

    设置部署基础路径

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/point-card',
+  assetPrefix: '/point-card',
 };
 
 export default nextConfig;
